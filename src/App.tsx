b7cfa3cc177d917f6ba5706cbe3f167139d0642b
WenKai Shi <73520560+eliajhmauve@users.noreply.github.com>
2026-03-09 21:52:09 +0800

    fix: add basename to Router for GitHub Pages subdirectory

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => {
         <Toaster />
         <Sonner />
         {showIntro && <OpeningAnimation onComplete={handleIntroComplete} />}
-        <BrowserRouter>
+        <BrowserRouter basename="/celestial-compass-12">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/input" element={<BaZiInputPage />} />
